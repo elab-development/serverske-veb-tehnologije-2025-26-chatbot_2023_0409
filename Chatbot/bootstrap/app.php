fd1dfd915d32d350301f769c6cc4da4da173c8d8
Ilija <ivanovicilija04@gmail.com>
2026-08-31 14:03:16 +0200

    apl

--- Chatbot/bootstrap/app.php
+++ Chatbot/bootstrap/app.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
+use App\Http\Middleware\RoleMiddleware;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -14,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+
+         $middleware->alias(['role' => RoleMiddleware::class,]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
